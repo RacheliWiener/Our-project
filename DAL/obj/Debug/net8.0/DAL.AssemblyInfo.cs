c7601d876a93994c0858635ca99f5cfa2ffc0ff8
RacheliWiener <winracheli@gmail.com>
2025-01-14 19:10:42 +0200

    try_commit_phsh

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af8e1a37aaad9b5a016b513f0b79af0da41d0bcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ff53b39213aa5335213c833e697c60d899ed7e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
